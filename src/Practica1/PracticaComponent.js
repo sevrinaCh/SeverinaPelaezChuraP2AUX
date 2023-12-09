7ba1d16472f2448022db50418817651b7e9805a1
Severina <sevrinaCh@gmail.com>
2023-12-08 20:13:23 -0400

    Exercise N2 Arrow Funtions

--- src/Practica1/PracticaComponent.js
+++ src/Practica1/PracticaComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Pregunta1 } from "./components/Pregunta1";
-
+import { Pregunta2 } from "./components/Pregunta2";
 export const PracticaComponent = () => {
   return (
     <div>
       <center><h1 >Practica # 1 ayudantia</h1></center>
       <Pregunta1/>
-      
+      <hr/>
+      <Pregunta2/>
     </div>
   );
 }
---
+++ src/Practica1/components/Pregunta2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Pregunta2 =(a,b)=> a/b;
+alert("Pregunta 2 :  "+ Pregunta2(10,2))
+
