26a34a68c1e4f4b8669f5fd3b7562bd25d7f7be4
Severina <sevrinaCh@gmail.com>
2023-12-08 20:07:25 -0400

    Exercise N1 Functions

---
+++ src/Practica1/PracticaComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Pregunta1 } from "./components/Pregunta1";
+
+export const PracticaComponent = () => {
+  return (
+    <div>
+      <center><h1 >Practica # 1 ayudantia</h1></center>
+      <Pregunta1/>
+      
+    </div>
+  );
+}
---
+++ src/Practica1/components/Pregunta1.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Pregunta1(a) {
+    return a**2;
+  }
+  
+  let result = Pregunta1(9);
+  alert("Pregunta 1 :  "+ result );
+  
